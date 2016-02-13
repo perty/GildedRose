632c6e1e811b2667a6adef5d123cac3946fe92ed
Per Lundholm <per@artcomputer.se>
2016-02-13 17:38:13 +0100

    Added skeleton test methods.

--- src/test/java/se/crisp/edu/refactor/gildedrose/GildedRoseTest.java
+++ src/test/java/se/crisp/edu/refactor/gildedrose/GildedRoseTest.java
@@ -13,4 +13,65 @@ public class GildedRoseTest {
         assertTrue(true);
     }
 
+    @Test
+    public void when_sell_by_date_has_passed_quality_degrades_twice_as_fast() throws Exception {
+
+
+    }
+
+    @Test
+    public void the_quality_is_never_negative() throws Exception {
+
+
+    }
+
+    @Test
+    public void aged_bries_increases_in_quality_as_it_gets_older() throws Exception {
+
+
+    }
+
+    @Test
+    public void quality_never_reaches_more_than_50() throws Exception {
+
+
+    }
+
+    @Test
+    public void sulfuras_legendary_item_never_has_to_be_sold() throws Exception {
+
+
+    }
+
+    @Test
+    public void backstage_passes_increases_in_quality() throws Exception {
+
+
+    }
+
+    @Test
+    public void backstage_passes_increases_by_2_in_quality_when_10_days_or_less() throws Exception {
+
+
+    }
+
+    @Test
+    public void backstage_passes_increases_by_3_in_quality_when_5_days_or_less() throws Exception {
+
+
+    }
+
+    @Test
+    public void backstage_passes_quality_drops_to_0_after_the_concert() throws Exception {
+
+
+    }
+
+    // New requirement
+
+    @Test
+    public void conjured_degrades_twice_as_fast_as_normal_items() throws Exception {
+
+
+    }
 }
