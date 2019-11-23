2c648664dd817da5a194a22105216d91eb20eda5
Per Lundholm <per@artcomputer.se>
2019-11-23 16:02:15 +0100

    Added first skeleton for Concordian

---
+++ java/src/test/java/specification/Requirements.java
@@ -0,0 +1,10 @@
+package specification;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class Requirements {
+
+    public String concordion;
+}
